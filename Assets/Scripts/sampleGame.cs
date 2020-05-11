7cceffb46f4cafe90bd22119f402d036e0b5ed10
MarcoKuo0132 <MarcoKuo0132@github.com>
2020-05-11 15:12:03 +0800

    update

---
+++ Assets/Scripts/sampleGame.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sampleGame : MonoBehaviour
+{
+    public GUISkin skin;    //GUISkin for GUI style controller
+    public GameObject tex;
+
+    private Rect lblRect;   //Rect for label
+    private Rect btnClearRect;   //Rect for Clear button
+    private Rect btnEndRect;   //Rect for End button
+
+    private List<GameObject> texs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lblRect = new Rect();
+        btnClearRect = new Rect();
+        btnEndRect = new Rect();
+
+        texs = new List<GameObject>();
+
+        lblRect.x = 0;
+        lblRect.y = Screen.height - 30;
+        lblRect.width = 100;
+        lblRect.height = 30;
+
+        btnClearRect.x = 0;
+        btnClearRect.y = 0;
+        btnClearRect.width = 100;
+        btnClearRect.height = 100;
+
+        btnEndRect.x = 0;
+        btnEndRect.y = 145;
+        btnEndRect.width = 100;
+        btnEndRect.height = 45;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.x /= Screen.width;
+            pos.y /= Screen.height;
+
+            GameObject g = (GameObject)Instantiate(tex, pos, Quaternion.identity);
+
+            texs.Add(g);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(lblRect, "0",skin.label);
+
+        if (GUI.Button(btnClearRect, "Clear", skin.button))
+        {
+            foreach (GameObject g in texs)
+            {
+                Destroy(g, 0);
+            }
+            texs.Clear();
+        }
+
+        if (GUI.Button(btnEndRect, "End", skin.button))
+        {
+            foreach (GameObject g in texs)
+            {
+                Destroy(g, 0);
+            }
+            texs.Clear();
+
+            sampleMenu m = gameObject.GetComponent<sampleMenu>();
+            m.enabled = true;
+            this.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/sampleMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sampleMenu : MonoBehaviour
+{
+    public GUIStyle btnStyle;
+
+    private Rect btnStartRect;       //rect for button
+    private Rect btnLeaveRect;       //rect for button
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnStartRect = new Rect();
+        btnLeaveRect = new Rect();
+
+        btnStartRect.x = Screen.width / 3;
+        btnStartRect.y = Screen.height * 2 / 5;
+        btnStartRect.width = Screen.width / 3;
+        btnStartRect.height = Screen.height / 5;
+
+        btnLeaveRect.x = Screen.width / 3;
+        btnLeaveRect.y = Screen.height * 2 / 5 - 50;
+        btnLeaveRect.width = Screen.width / 3;
+        btnLeaveRect.height = Screen.height / 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(btnStartRect, "Start", btnStyle))
+        {
+            sampleGame g = gameObject.GetComponent<sampleGame>();
+            g.enabled = true;
+            this.enabled = false;
+        }
+
+        if (GUI.Button(btnLeaveRect, "Leave", btnStyle))
+        {
+            Application.Quit();
+        }
+    }
+}
